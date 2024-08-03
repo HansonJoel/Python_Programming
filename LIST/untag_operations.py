1a2c69a361426786863d1fc852e4cfb11aa1c09e
HansonJoel <joelhanson002@gmail.com>
2024-08-03 13:21:46 +0100

    This operation allows you convert strings into a list variable

---
+++ LIST/untag_operations.py
@@ -0,0 +1,9 @@
+""" This operation untags each element as a seperate element in a list. see example below"""
+
+# syntax: variablename = list (a,b,.) where a,b are element
+
+var = list ("Hello there")
+print(var)
+
+
+""" from the output, you will see that each letter in the string "Hello there" has become a single element in a list var"""
