f5d722d771d8612ea75c55a9057ec8d28f74c7e7
HansonJoel <joelhanson002@gmail.com>
2024-08-03 11:30:53 +0100

    Accessing elements in a list

---
+++ LIST/accessing_element_in_a_list.py
@@ -0,0 +1,13 @@
+""" There is no point of creating a list, if you can't access individual elements in that list. To understand how we access element within list, lets do an example"""
+
+mix = [1,6,'simplilern', 'get', 'cerified']
+print (mix[3])
+
+
+# we can also access element in list from the backward position
+
+print(mix[-1])  #This will output certified
+print(mix[-2])  # This will output get
+
+
+"""Notice, when counting index in list from forward position (left to right), the first index start counting from 0,1,2 ...etc but when you are counting from backward position (from right to left), the index begins with -1,-2,-3...etc
