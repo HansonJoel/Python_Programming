4cfdb43b10c5963b06683e334d4d7e4d26e386d1
HansonJoel <joelhanson002@gmail.com>
2024-08-04 19:19:40 +0100

    i appended a number and string to a list

---
+++ LIST/METHOD_in_LIST/append.py
@@ -0,0 +1,17 @@
+"""
+Append: The append method enables you insert element you wish to add to an existing list.
+
+syntax: listname.append (a) 
+where a is the parameter/element to be added to the list
+Note: The append method parameter(a) only accepts one value
+"""
+num = [1,2,3,4]
+print(num)
+num.append(5)
+print(num)
+
+# You can also insert a string
+
+
+num.append("are numbers")
+print(num)
