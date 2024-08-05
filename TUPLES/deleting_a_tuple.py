909b6afac9d43afa40508d95bd5fb33d307139e1
HansonJoel <joelhanson002@gmail.com>
2024-08-05 15:58:59 +0100

    How to delete a tuple

---
+++ TUPLES/deleting_a_tuple.py
@@ -0,0 +1,13 @@
+"""
+Deleting Tuples: since tuples are immutable, it is impossible to delete the element within it but that doesnt mean you cannot delete the tuple entirely. we use the del keyword when deleting tuples
+
+SYNTAX: del(a)
+where a variable_name of tuple to be deleted
+"""
+
+# Example1: 
+tuple_1 = 1,2,3,4,5
+print (tuple_1)  # This outputs the values ot tuple_1
+
+del(tuple_1)     # This deletes tuple_1
+
