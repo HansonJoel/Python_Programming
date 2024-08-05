aa5ff84e8e808b24d6f7800007010844aa4e66c3
HansonJoel <joelhanson002@gmail.com>
2024-08-05 15:20:30 +0100

    How to concatenate tuples

---
+++ TUPLES/concatenation_in_tuple.py
@@ -0,0 +1,9 @@
+"""
+Though you can't modify tuples (because tuples are immutable),you can concatenate it.
+"""
+city = "Abuja","Lagos","Uyo","calabar"
+numbers = 1,2,3,4
+
+# Example1: concatenate the 2 list above
+print (city+numbers)
+
