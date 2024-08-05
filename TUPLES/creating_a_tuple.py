2713a4e895fa626aedd0d0ed8374eeff6c43e56b
HansonJoel <joelhanson002@gmail.com>
2024-08-05 10:21:41 +0100

    How to create a tuple

---
+++ TUPLES/creating_a_tuple.py
@@ -0,0 +1,21 @@
+"""
+Unlike list, in tuple, element are enclosed in curly brackets and seperated by commas eg numbers = (1,2,3,4,"london"). We can also write it without the curly bracket but there must be seperated by commas e.g number = 1,2,"a",4
+"""
+
+# empty tuple
+emp = ()
+print (type(emp))
+
+numbers = (1,2,3,4,"london") 
+print(numbers)
+
+
+city = "Abuja","Lagos","Uyo","calabar"
+print(city)
+
+
+"""
+Difference between list and Tuples
+1. List are mutable while tuples are immutable
+2. List uses square brackets [], why tuples may or may not use parenthesis()
+"""
