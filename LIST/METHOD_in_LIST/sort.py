9c9c00096408954c5099e883e7fd6cb21ffc22db
HansonJoel <joelhanson002@gmail.com>
2024-08-05 09:35:07 +0100

    How to sort elements in list

---
+++ LIST/METHOD_in_LIST/sort.py
@@ -0,0 +1,13 @@
+"""
+sort: This method allows you arrange things in an orderly manner
+
+SYNTAX: listname.sort(a)
+where a is the list to be sorted
+
+Example: we want to sort this list
+var1 = ["b","f","a","q","f"]
+"""
+
+var1 = ["b","f","a","q","f","c","e","d"]
+var1.sort()
+print(var1)
