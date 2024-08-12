6b423893872b2a731225ac803d91c7df46a8a234
HansonJoel <joelhanson002@gmail.com>
2024-08-12 15:51:43 +0100

    How to create dictionaries

---
+++ DICTIONARIES/creating_dictionaries.py
@@ -0,0 +1,18 @@
+# How to create a dictionary
+# Dictinaries are enclosed within curly braces {}
+
+d1 = {}        #This is an empty dictinary without elements
+print(d1)
+print(type(d1))
+
+
+
+"""
+There is another method of creating dictionary which is using the dict method. This method is notostly needed as the method above is most preferrable.
+when using the dict method, ensure you have not named any variable as dict, else there will be an error.
+"""
+
+d2 = ({})
+print(d2)
+print(type(d2))
+
