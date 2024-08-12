ae2a48fb08fdc609f4d0a2a1d1fcc80a17693982
HansonJoel <joelhanson002@gmail.com>
2024-08-12 16:39:48 +0100

    How to add element to an empty dictionary

---
+++ DICTIONARIES/adding_element_to_dictionary.py
@@ -0,0 +1,17 @@
+# How to add elements to dictionaries
+""" 
+created am empty dictionary called (d)
+where we are going to add element
+"""
+
+d ={} # This is an empty dictionary
+
+d[0] = "Welcome," # [0] is the key, "welcome" is the value
+print(d)
+
+d[1]= (" what is your name: ") # added a new element
+print(d)
+
+d["Name:"] = "Sam"  # dictionary with key as string
+print(d)
+
