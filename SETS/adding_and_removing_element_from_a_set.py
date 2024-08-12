0991052e4353a1f40ef67997fed908294b865b1e
HansonJoel <joelhanson002@gmail.com>
2024-08-13 00:46:45 +0100

    How to add and remove element from a set

---
+++ SETS/adding_and_removing_element_from_a_set.py
@@ -0,0 +1,17 @@
+# How to add element to a set
+# SYNTAX: variablename.add(a)
+# where a is the element to be added
+
+set_numbers= {1,2,3,4}
+print(set_numbers)
+
+set_numbers.add(5)  # added 5 to the sequence of numbers
+print(set_numbers)
+
+
+# How to remove element from a set
+# SYNTAX: variablename.remove(a)
+# where a is the element to be added
+
+set_numbers.remove(3)  #removes 3 from the sequence of numbers
+print(set_numbers) 
