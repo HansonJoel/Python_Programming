025309c33eb4d7f2a1d065d336cf5994781d0baa
HansonJoel <joelhanson002@gmail.com>
2024-08-12 16:27:08 +0100

    How to create a dictionary within a dictionary

---
+++ DICTIONARIES/Nested_Dictionaries.py
@@ -0,0 +1,11 @@
+# Nested Dictionary
+This is a creation of a dictionary within a dictionary. 
+
+d3 = {"name":"Sam","Age":32, "Profession":"Surveyor"} 
+if our key "name" has 2 values i.e firstname and lastname, in this case, "name" can be the key to another dictionary where the first name and lastname can be stored. d3 now becomes
+"""
+
+d3 = {"name":{"Firstname":"Sam","Lastname":"Crew"},"Age":32, "Profession":"Surveyor"}
+print(d6)
+
+# from the code above we have assigned firstname and lastname values to the key "name"
