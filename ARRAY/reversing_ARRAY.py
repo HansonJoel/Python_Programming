b4799ece0d7136248bad3ea1542229c1cd62f764
HansonJoel <joelhanson002@gmail.com>
2024-09-04 09:10:42 +0100

    How to reverse element in your array

---
+++ ARRAY/reversing_ARRAY.py
@@ -0,0 +1,12 @@
+# Reversing an array
+"""
+You can reverse element in  your array using the reverse function
+syntax: variable_name.reverse()
+"""
+
+from array import * # always import array
+arr = array ('i',[1,2,3,4])
+print (arr)
+
+arr.reverse()
+print(arr)
