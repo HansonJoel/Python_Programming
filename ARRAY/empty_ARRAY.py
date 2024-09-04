8c9b3a55d6ee34bc6e6a65deaff294db98034734
HansonJoel <joelhanson002@gmail.com>
2024-09-04 08:54:43 +0100

    How to create an empty array

---
+++ ARRAY/empty_ARRAY.py
@@ -0,0 +1,8 @@
+# How to create an empty array to contain signed integer
+# ensure you always import array
+# syntax : variable_name ('type_code', [ ]) 
+
+
+from array import *
+arr = array('i', [ ])
+print(arr)  # This is an empty array
