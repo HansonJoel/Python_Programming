37cfda0674f7aab96d9ee1280bf3664b32df0c98
HansonJoel <joelhanson002@gmail.com>
2025-02-10 17:49:59 +0100

    Program to calculate Total Price with Discount

---
+++ Python Exercises/Functions/calculate_total_price_with_discount.py
@@ -0,0 +1,27 @@
+'''
+💡 Scenario: You are developing an online shopping platform. Create a function that calculates the total price after applying a discount of 10% for purchases greater than 500
+'''
+
+def calculate_total_price(price, discount_percentage=0):
+    return price - ((discount_percentage/100)*price)
+    
+
+price = int(input("Kindly enter your Bill: "))
+# eligibility= input("Does Customer qualify for discount (Y/N): ")
+
+discount_percentage = 0
+if price >= 500:
+    discount_percentage = 10
+    print("You have a 10% discount")
+    
+else:
+    print("Sorry, you do not qualify for a discount")
+
+
+Total_Bill = calculate_total_price(price, discount_percentage)
+print(f"Your Total Bill is ${Total_Bill}")
+
+
+
+
+    
