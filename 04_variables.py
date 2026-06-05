356ea8cf9c6f4c6c47791b7486904864e231fb97
HansonJoel <joelhanson002@gmail.com>
2026-06-05 12:28:54 +0100

    variables

---
+++ 04_variables.py
@@ -0,0 +1,8 @@
+# Vraiables are container for storing values.
+# The values stored in the container can vary at anytime
+
+
+name = input("What is your name? ")
+length = len(name)         # This calculates the length of the name value you entered
+print(name)
+print('Your name has'+' ', length,'Characters', sep=",")
