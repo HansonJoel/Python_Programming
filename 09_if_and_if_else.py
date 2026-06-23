88c1dcc95eb65edbcf283991a453e774c7ef699a
HansonJoel <joelhanson002@gmail.com>
2026-06-23 12:12:11 +0100

    if and if else statement

---
+++ 09_if_and_if_else.py
@@ -0,0 +1,15 @@
+# if condition
+age = 18
+if age >= 18:
+    print("You are eligible to Vote")
+
+
+# if-else
+age = int(input(f'Kindly enter your age: '))
+if age >= 18:
+    print('You are eligible to Vote')
+    print('Thank You')
+else:
+    print('You are not eligible to Vote')
+    print ('Sorry')
+
