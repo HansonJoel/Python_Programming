fc3d744633727f679b3059253e7ded0f4724fad4
HansonJoel <joelhanson002@gmail.com>
2026-06-23 12:54:37 +0100

    program to check if number is even or odd

---
+++ Practicals/06_program to check if number is even or odd.py
@@ -0,0 +1,11 @@
+# write a program that request for input from a user and checks if the inputted number is even or odd number
+
+number = int(input('Kindly enter a number: '))
+
+if number % 2 == 0:
+    print(f"{number}, is an Even Number")
+else:
+    print(f'{number}, is an Odd Number')
+
+
+
